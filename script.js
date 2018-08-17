48de22b4f0015c46efe7085bfad8332c89a2b4c1
Zsolt Eskulits <eskzsolt@gmail.com>
2018-08-17 17:04:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     const cnt = ($('#' + id).data('counter')) + direction;
     ($(`#${id}`).data('counter', cnt));
 
-    const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt)/4)*4 : cnt > 3 ? cnt - Math.floor(cnt/4)*4 : cnt;
+    const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt)/4)*4 : cnt > 3 ? cnt - ~~(cnt/4)*4 : cnt;
 
     $($(`#${id}`).find('.symbol')).css({'transform': (['_', 'p', 't', 'r', 'f', 'th'].includes(id) ? `rotate(${cnt*90}deg)` : `scale(${transf[pos]})`) });
     $($(`#${id}`).find('.transcription')).text((id === '_' ? '' : id) + ['i', 'o', 'e', 'a'][pos])
