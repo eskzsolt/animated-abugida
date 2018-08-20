092c3ac7f92827dbc2f805fb32f7515943a8f35b
Zsolt Eskulits <eskzsolt@gmail.com>
2018-08-21 00:16:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
   function adjust(id, direction) {
 
+    const toRotate = ['_', 'p', 't', 'r', 'f', 'th'];
     const transf = ['1, 1', '-1, 1', '-1, -1', '1, -1'];
+    const vowels = ['i', 'o', 'e', 'a'];
 
     if (($(`#${id}`).data('counter')) === undefined) ($(`#${id}`).data('counter', 0));
     const cnt = ($('#' + id).data('counter')) + direction;
@@ -10,8 +12,8 @@
 
     const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt)/4)*4 : cnt > 3 ? cnt - ~~(cnt/4)*4 : cnt;
 
-    $($(`#${id}`).find('.symbol')).css({'transform': (['_', 'p', 't', 'r', 'f', 'th'].includes(id) ? `rotate(${cnt*90}deg)` : `scale(${transf[pos]})`) });
-    $($(`#${id}`).find('.transcription')).text((id === '_' ? '' : id) + ['i', 'o', 'e', 'a'][pos])
+    $($(`#${id}`).find('.symbol')).css({'transform': (toRotate.includes(id) ? `rotate(${cnt*90}deg)` : `scale(${transf[pos]})`) });
+    $($(`#${id}`).find('.transcription')).text((id === '_' ? '' : id) + vowels[pos])
 
   }
 
