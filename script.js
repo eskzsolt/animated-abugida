374885e36f16cf9f2af0508f889b54e983c74c19
Zsolt Eskulits <eskzsolt@gmail.com>
2018-08-21 16:38:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@
 
     const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt)/4)*4 : cnt > 3 ? cnt - ~~(cnt/4)*4 : cnt;
 
-    $($(`#${id}`).find('.symbol')).css({
-      'transform': (['_', 'p', 't', 'r', 'f', 'th'].includes(id) ?
-      `rotate(${cnt*90}deg)` :
-      `scale(${['1, 1', '-1, 1', '-1, -1', '1, -1'][pos]})`)
-    });
+    $($(`#${id}`).find('.symbol')).css({'transform': (
+        ['_', 'p', 't', 'r', 'f', 'th'].includes(id) ?
+        `rotate(${cnt*90}deg)` :
+        `scale(${['1, 1', '-1, 1', '-1, -1', '1, -1'][pos]})`
+    )});
     $($(`#${id}`).find('.transcription')).text((id === '_' ? '' : id) + ['i', 'o', 'e', 'a'][pos])
 
   }
