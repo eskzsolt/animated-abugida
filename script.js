04659e5e10e1aadaf5d6d28d9d5a9fae64b57e3a
Zsolt Eskulits <eskzsolt@gmail.com>
2020-06-28 11:36:01 +0200

    Made standardJS compliant

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-(function() {
+(function () {
+  const $ = window.$
 
-  function adjust(id, direction) {
-
-    if (($(`#${id}`).data('counter')) === undefined) ($(`#${id}`).data('counter', 0));
+  function adjust (id, direction) {
+    if (($(`#${id}`).data('counter')) === undefined) ($(`#${id}`).data('counter', 0))
     const cnt = ($('#' + id).data('counter')) + direction;
-    ($(`#${id}`).data('counter', cnt));
+    ($(`#${id}`).data('counter', cnt))
 
-    const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt)/4)*4 : cnt > 3 ? cnt - ~~(cnt/4)*4 : cnt;
+    const pos = cnt < 0 ? cnt + Math.ceil(Math.abs(cnt) / 4) * 4 : cnt > 3 ? cnt - ~~(cnt / 4) * 4 : cnt
 
-    $($(`#${id}`).find('.symbol')).css({'transform': (
-        ['_', 'p', 't', 'r', 'f', 'th'].includes(id) ?
-        `rotate(${cnt*90}deg)` :
-        `scale(${['1, 1', '-1, 1', '-1, -1', '1, -1'][pos]})`
-    )});
+    $($(`#${id}`).find('.symbol')).css({
+      transform: (
+        ['_', 'p', 't', 'r', 'f', 'th'].includes(id)
+          ? `rotate(${cnt * 90}deg)`
+          : `scale(${['1, 1', '-1, 1', '-1, -1', '1, -1'][pos]})`
+      )
+    })
     $($(`#${id}`).find('.transcription')).text((id === '_' ? '' : id) + ['i', 'o', 'e', 'a'][pos])
-
   }
 
-  $(function() {
-      $('.symbol').on({
-        click: function() {
-          adjust($(this).parent()[0].id, -1);
-        },
-        contextmenu: function() {
-          adjust($(this).parent()[0].id, 1);
-          return false;
-        }
-      });
-  });
+  $(function () {
+    $('.symbol').on({
+      click: function () {
+        adjust($(this).parent()[0].id, -1)
+      },
+      contextmenu: function () {
+        adjust($(this).parent()[0].id, 1)
+        return false
+      }
+    })
+  })
 
-  document.querySelector('video').playbackRate = 0.2;
-
-})();
+  document.querySelector('video').playbackRate = 0.2
+})()
